5ecdc8fdd3c530c788b758025d385a173f0695d7
전지우 <jjiwoo28@gmail.com>
2023-09-21 14:14:35 +0900

    Hello for 문 추가

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -3,7 +3,10 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello,Wolrd");
 		System.out.print("Hello,World");
-		System.out.println("Difference between println and print");		
+		System.out.println("Difference between println and print");
+		for(int i = 0; i< 100; i++) {
+			System.out.println("test",i);
+		}
 	}
 
 }
